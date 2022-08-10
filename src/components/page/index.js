e2f734353efbce1e5ba17cb8187bb3d9641efb9a
Sihkdragon <ditolord@gmail.com>
2022-08-10 07:56:54 +0700

    Some Change responsiveness in layout.js

--- src/components/page/index.js
+++ src/components/page/index.js
@@ -3,7 +3,7 @@ import MainLayout from "../layout";
 
 const Page = () => {
   return (
-    <div className="w-full h-full 2xl:w-[1400px] 2xl:h-[800px] bg-mainbg m-auto px-10 bg-origin-padding">
+    <div className="w-full h-full 2xl:w-[1400px] 2xl:h-[800px] bg-mainbg  m-auto px-10 bg-cover">
       <MainLayout />
     </div>
   );
