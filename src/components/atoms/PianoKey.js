eb908c68689a32a0e4e1209f0c102fa1074719cb
Sihkdragon <ditolord@gmail.com>
2022-08-14 20:46:05 +0700

    add button function for keys

--- src/components/atoms/PianoKey.js
+++ src/components/atoms/PianoKey.js
@@ -1,26 +1,34 @@
 import { useAtom } from "jotai";
+import { useEffect } from "react";
+import { useState } from "react";
 import { key_to_note } from "../../global/constant";
 import { isLetterDisplayed } from "../../global/state";
 
-const PianoKey = ({ note, indeks }) => {
+const PianoKey = ({ note, indeks, pressedKeys }) => {
   const [letterDisplayed] = useAtom(isLetterDisplayed);
-  if (note.length > 2) {
-    return (
-      <div className=" cursor-pointer relative z-10 -mx-4 bg-black text-white h-4/5 w-8  rounded-b text-center hover:bg-slate-900">
-        <span className="absolute bottom-2 uppercase font-semibold left-3">
-          {!letterDisplayed
-            ? ""
-            : key_to_note.find((keynote) => {
-                return keynote.from === note;
-              })
-            ? key_to_note[indeks - 19].to
-            : " "}
-        </span>
-      </div>
-    );
-  }
+  const [className, setClassName] = useState("pianokey");
+  const [PressedclassName, setPressedClassName] = useState(false);
+
+  const onKeyDownHandler = () => {
+    setPressedClassName(true);
+  };
+  const onKeyUpHandler = () => {
+    setPressedClassName(false);
+  };
+
+  useEffect(() => {
+    console.log(pressedKeys);
+    if (note.length > 2) {
+      setClassName("pianokeyflat");
+    }
+  }, []);
+
   return (
-    <div className="relative cursor-pointer bg-white text-black h-full w-11 border-black border-x border-b rounded-sm text-center hover:bg-slate-300">
+    <button
+      className={`${className} ${PressedclassName ? "pressed" : ""}`}
+      onMouseDown={onKeyDownHandler}
+      onMouseUp={onKeyUpHandler}
+    >
       <span className="absolute bottom-2 uppercase font-semibold left-4">
         {!letterDisplayed
           ? ""
@@ -30,7 +38,7 @@ const PianoKey = ({ note, indeks }) => {
           ? key_to_note[indeks - 19].to
           : " "}
       </span>
-    </div>
+    </button>
   );
 };
 
--- src/components/organisms/Piano.js
+++ src/components/organisms/Piano.js
@@ -1,12 +1,21 @@
 import React from "react";
 import PianoKey from "../atoms/PianoKey";
 import { key } from "../../global/constant";
+import { useState } from "react";
 
 const Piano = () => {
+  const [keyPressed, setKeyPressed] = useState(["X"]);
   return (
     <div className="rounded-t-sm bg-white h-full flex">
       {key.map((key, index) => {
-        return <PianoKey note={key} key={index} indeks={index} />;
+        return (
+          <PianoKey
+            note={key}
+            key={index}
+            indeks={index}
+            pressedKeys={keyPressed}
+          />
+        );
       })}
     </div>
   );
